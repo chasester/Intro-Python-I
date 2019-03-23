f6355959453bffd145f9bdd3e622564cf3dd6677
Chase Wex <clapingmanly@hotmail.com>
2019-03-22 20:31:53 -0400

    Added a very clear cal.py calendar.
    Fixed a few mistakes from other modules
    
    Note: Finished module.
    
    Signed-off-by: Chase Wex <clapingmanly@hotmail.com>

--- src/cal.py
+++ src/cal.py
@@ -18,7 +18,70 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 """
+#adjusted this so that you can enter only the year by
+#typing andything then a year (followed by a space) it
+#will print curmonth useryear.
+#This is more user friendly and was much harder to do.
+#It also handles 3 letter abrivation and full name months
+#years auto convert from 10-99 to 2010-2099 and 0010-0099
+#how you call dates in this range.
+#also made this a loop so it will keep repeating till the
+#user types 'q' which it is only told after the first cycle.
 
 import sys
 import calendar
 from datetime import datetime
+
+date = datetime.date(datetime.today());
+cal = calendar.TextCalendar();
+
+def handle_month(m):
+    try:
+        int(m);
+        if(int(m) < 1 or int(m) > 12):
+            raise;
+    except:
+        try:
+            m = stringmonths[m];
+        except:
+            m = -1;
+    return m;
+
+def handle_year(y):
+    try:
+        if(int(y) > 9999 or int(y) < 1):
+            raise;
+        if(len(y) == 2):
+            y = "20" + y;
+    except:
+        y =  -1;
+    return y;
+            
+while (True):
+    stringmonths = { "jan":1, "january":1, "feb":2, "february":2, "mar":3, "march":3, "apr":4, "april":4, "may":5, "jun":6, "june":6, "jul":7, "july":7, "aug":8, "august":8, "sep":9, "september":9, "oct":10, "october":10, "nov":11, "november":11, "dec":12, "december":12 }
+
+    userinput = input("Please Enter a Month and/or Year: ").lower().split(" ");
+    if(userinput[0] == "q"):
+        break;
+    date = datetime.date(datetime.today());
+    cal = calendar.TextCalendar();
+    if len(userinput) > 0:
+        userinput[0] = handle_month(userinput[0]);
+    else:
+        userinput.append(-1);
+    if len(userinput) > 1:
+        userinput[1] = handle_year(userinput[1])
+    else:
+        userinput.append(-1);
+    
+    if(userinput[0] == -1 or userinput[1] == -1):
+        if(userinput[0] == -1 and userinput[1] == -1):
+            print("\n\nPlease enter month followed by year. \nValid Month sytax: (1-12 or Jan - Dec or January - December)\nVaild Year format (1-9999 years 10-99 become 2010-2099 use 0010-0099 for dates in that range).\nIf you wish to get the current month of a given year type '*' then year.\n\n");
+            continue;
+        if(userinput[0] == -1):
+            userinput[0] = date.month;
+        if(userinput[1] == -1):
+            userinput[1] = date.year;
+
+    print("\n\n"+cal.formatmonth(int(userinput[1]), int(userinput[0])));
+    print("\nType \'q\' to quit");
--- src/classes.py
+++ src/classes.py
@@ -2,20 +2,46 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    lat = 0;
+    lon = 0;
+    def __init__(self, _lat, _lon):
+        self.lat = _lat;
+        self.lon = _lon;
+    def __str__(self):
+        return (f"Lat: {self.lat}, Lon: {self.lon}");
         
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    name = "";
+    def __init__(self, _name,_lat,_lon):
+        self.name = _name;
+        LatLon.__init__(self,_lat,_lon);
+    def __str__(self):
+        return (f"Name: {self.name}, {LatLon.__str__(self)}");
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    difficulty = 0;
+    size = (0,0);
+    def __init__(self,_name,_difficulty,_size,_lat,_lon):
+        self.difficulty = _difficulty;
+        self.size = _size;
+        Waypoint.__init__(self,_name,_lat,_lon);
+    def __str__(self):
+        return (f"{Waypoint.__str__(self)} Size: {self.size}, Difficulty: {self.difficulty}");
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+
+waypoint = Waypoint("Catacombs",41.70505, -121.51521);
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
@@ -24,6 +50,7 @@ print(waypoint)
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137,-121.41556);
 
 # Print it--also make this print more nicely
 print(geocache)
